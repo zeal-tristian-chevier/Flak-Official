b48e93774543e6be33f817b9d92d61cc07b90beb
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-08-25 17:24:41 -0600

    update

--- flask_app/static/animations/animate.js
+++ flask_app/static/animations/animate.js
@@ -3,14 +3,19 @@ const cards = document.querySelectorAll('.card-animate')
 window.addEventListener('scroll', checkCards)
 
 function checkCards() {
-    const trigger = window.innerHeight
+    const trigger = window.innerHeight + 200;
 
-    cards.forEach(card => {
-        const cardTop = card.getBoundingClientRect().top
-        let currentCard = 
-        cardTop < trigger ?  
-        card.classList.remove('hide') : 
-        card.classList.add('hide')
-        return currentCard
-    })
+    if(screen.width < 1024){
+        cards.forEach(card => {
+            const cardTop = card.getBoundingClientRect().top
+            let currentCard = 
+            cardTop < trigger ?  
+            card.classList.remove('animate__backInRight') : 
+            card.classList.add('animate__backInRight')
+            return currentCard
+        })
+    }else{
+        card.classList.remove('animate__backInRight')
+    }
+    
 }
