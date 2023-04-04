0a97b98b373230b9a020cf8082b7d1ccde0e5d78
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2023-04-04 00:33:44 -0600

    connected deployed clever cloud db

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -1,12 +1,13 @@
 # a cursor is the object we use to interact with the database
 import pymysql.cursors
+from decouple import config
 # this class will give us an instance of a connection to our database
 class MySQLConnection:
     def __init__(self, db):
         # change the user and password as needed
-        connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', 
-                                    password = 'passwordpassword', 
+        connection = pymysql.connect(host = config('SQL_HOST'),
+                                    user = config('SQL_USER'), 
+                                    password = config('SQL_PASSWORD'), 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
